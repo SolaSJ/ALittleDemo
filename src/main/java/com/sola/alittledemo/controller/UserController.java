df47551a75971c2edc27c8dcb19aad26836c63eb
Sola <solasj@qq.com>
2018-08-05 21:55:24 +0800

    first commit

---
+++ src/main/java/com/sola/alittledemo/controller/UserController.java
@@ -0,0 +1,18 @@
+package com.sola.alittledemo.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class UserController {
+
+    @GetMapping("/user/{userId}")
+    public Object getUser(@PathVariable Long userId) {
+        log.info("查询用户, 用户id: {}", userId);
+        return "查询用户, 用户id: " + userId;
+    }
+
+}
